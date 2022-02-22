f355122d34abea09332cfdf33a9dc2909f700467
Fernando Mercado <fermercado@live.com>
2022-02-22 20:16:39 -0300

    boolean e condicionais

---
+++ Boolean e Condicionais/script.js
@@ -0,0 +1,28 @@
+// let possuiGraduacao = false
+// let possuiDoutorado = false
+
+// if (possuiGraduacao) {
+//   console.log(' E verdadeiro')
+// } else if (possuiDoutorado) {
+//   console.log('Possui Doutorado')
+// } else {
+//   console.log('Nao possui nada')
+// }
+
+// let nome = 'Fernando'
+
+// if (nome) {
+//   console.log(nome)
+// } else {
+//   console.log('Nome nao existe')
+// }
+
+let corFavorita = 'azul'
+
+switch (corFavorita) {
+  case 'azul':
+    console.log('Olhe para o ceu')
+    break
+  default:
+    console.log('Fecha os olhos')
+}
