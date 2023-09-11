0f182d94198e97ee5f2093ccd2695f08f4b981a1
Fernando Mercado <fermercado@live.com>
2023-09-11 10:20:26 -0300

    docs: operadores

---
+++ 2023/Operadores/script.js
@@ -0,0 +1,59 @@
+//1- Number
+
+//2- Operações Aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+console.log(5 + 4 * 12);
+
+//3  - Special Numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+//4 - Strings
+console.log("Um texto");
+console.log("123");
+console.log(typeof "Um texto");
+console.log(typeof "123");
+
+//5- Símbolos especiais em string
+console.log("testando a \nquebra de linha");
+console.log("\ttestando a tab");
+
+//6- Concatenação
+console.log("Oi," + " tudo" + " bem?");
+
+//7- Template Strings
+console.log(`A soma de 2+2 é = ${2 + 2}`);
+
+//8- Booleans
+console.log(true);
+console.log(false);
+console.log(5 > 20);
+console.log(5 < 20);
+
+//9- Comparações
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 == 9);
+console.log(10 != 9);
+
+//10- Identico
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 != "9");
+console.log(9 !== "9");
+
+//11 - Operadores Lógicos
+
+console.log(true && true);
+console.log(true && false);
+
+//13 - mudança de tipos
+console.log(5 * null);
+console.log("Oi" * "Tudo bem");
+console.log("10" + 1)
+console.log("10" - 1)
